9bf54557510674e73713f9daeeb72e20c89f7412
James Turello <jdturello@gmail.com>
2016-02-12 09:59:05 -0800

    add passing unit test for condition lower case vowels - aeiou - change to dashes

--- src/test/java/AppTestUnit.java
+++ src/test/java/AppTestUnit.java
@@ -1,13 +1,12 @@
-// Unit Tests
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class AppTestUnit {
 
   @Test
-  public void method_forBehavior_expectedResult() {
-    // eventually put your testing code here
-    assertEquals(true, true);
+  public void vowelsToDash_inputStringHasLowerCaseVowels_aeiou_vowelsChangedToDashes() {
+    App app = new App();
+    assertEquals("Th-s -s - str-ng w-th v-w-ls - ----- -r -----", app.vowelsToDash("This is a string with vowels - aeiou or AEIOU"));
   }
 
 }
