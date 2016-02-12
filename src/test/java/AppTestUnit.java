cca1735d86f88f2f9c80ed6223f10e844dae9820
James Turello <jdturello@gmail.com>
2016-02-12 10:06:43 -0800

    add passing unit test for condition upper case vowels - AEIOU - change to dashes

--- src/test/java/AppTestUnit.java
+++ src/test/java/AppTestUnit.java
@@ -6,7 +6,13 @@ public class AppTestUnit {
   @Test
   public void vowelsToDash_inputStringHasLowerCaseVowels_aeiou_vowelsChangedToDashes() {
     App app = new App();
-    assertEquals("Th-s -s - str-ng w-th v-w-ls - ----- -r -----", app.vowelsToDash("This is a string with vowels - aeiou or AEIOU"));
+    assertEquals("Th-s -s - str-ng w-th v-w-ls - -----", app.vowelsToDash("This is a string with vowels - aeiou"));
+  }
+
+  @Test
+  public void vowelsToDash_inputStringHasUpperCaseVowels_AEIOU_vowelsChangedToDashes() {
+    App app = new App();
+    assertEquals("TH-S -S - STR-NG W-TH V-W-LS - -----", app.vowelsToDash("THIS IS A STRING WITH VOWELS - AEIOU"));
   }
 
 }
